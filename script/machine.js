2db91776020eb71fd3c8f9ccee4acda408729c70
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-02-23 23:57:04 +0600

    when the button all clicked all the card  is displayed

--- script/machine.js
+++ script/machine.js
@@ -2,6 +2,7 @@
 function hideAllClasses(id) {
   //get the all class means cards
   const interview = document.getElementById("interview-section");
+  const rejected = document.getElementById("rejected-section");
   const card1 = document.getElementById("card-1");
   const card2 = document.getElementById("card-2");
   const card3 = document.getElementById("card-3");
@@ -12,6 +13,7 @@ function hideAllClasses(id) {
   const card8 = document.getElementById("card-8");
   //hide all the classes
   interview.classList.add("hidden");
+  rejected.classList.add("hidden");
   card1.classList.add("hidden");
   card2.classList.add("hidden");
   card3.classList.add("hidden");
@@ -20,9 +22,20 @@ function hideAllClasses(id) {
   card6.classList.add("hidden");
   card7.classList.add("hidden");
   card8.classList.add("hidden");
- 
 
-  // show only the class that has been clicked
-  const showClass = document.getElementById(id);
-  showClass.classList.remove("hidden");
+  //checks if all btn is pressed or not
+  if (id === "all") {
+    card1.classList.remove("hidden");
+    card2.classList.remove("hidden");
+    card3.classList.remove("hidden");
+    card4.classList.remove("hidden");
+    card5.classList.remove("hidden");
+    card6.classList.remove("hidden");
+    card7.classList.remove("hidden");
+    card8.classList.remove("hidden");
+  } else {
+    // show only the class that has been clicked
+    const showClass = document.getElementById(id);
+    showClass.classList.remove("hidden");
+  }
 }
