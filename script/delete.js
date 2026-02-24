065d7c673301b7adec725aab46d94edbbc91c4db
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-02-24 19:46:40 +0600

    question and answers

--- script/delete.js
+++ script/delete.js
@@ -1,14 +1,19 @@
-// //get the main container
-// const mainContainer = document.querySelector("main");
-// // console.log(mainContainer);
+//get the main container
+const mainContainer = document.querySelector("main");
+// console.log(mainContainer);
 
-// mainContainer.addEventListener("click", function (event) {
-//   // Check if the clicked element is a delete button or its child icon
-//   if (event.target.classList.contains('fa-trash') || event.target.closest('.btn-circle')) {
-//     // Find the card parent (the div with id like "card-1")
-//     const card = event.target.closest('.card');
-//     if (card) {
-//       card.remove();
-//     }
-//   }
-// });
+mainContainer.addEventListener("click", function (event) {
+  // Check if the clicked element is a delete button or its child icon
+  if (
+    event.target.classList.contains("fa-trash") ||
+    event.target.closest(".btn-circle")
+  ) {
+    // Find the card parent (the div with id like "card-1")
+    const card = event.target.closest(".card");
+    if (card) {
+      card.remove();
+    }
+  }
+});
+
+
